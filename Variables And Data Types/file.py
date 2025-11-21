3c6af5638cc540e4bbea72f9aa28da9a038d8d91
shravan098 <shravanjadhav041@gmail.com>
2025-11-21 15:54:41 +0530

    Add initial implementation of variables and data types in file.py

---
+++ Variables And Data Types/file.py
@@ -0,0 +1,20 @@
+#variables
+a=1
+#Data Types
+b="String"
+c=1#Integer
+d=20.9#Float
+E=True#Boolean
+F=[1,23,34,3]#List
+# if we want to add any element in the list the 
+F.append(2)
+F.extend([32,342,432,432,5])
+F.insert(0,"Shravan") # index 0 par Shravan add hoga
+print(F)
+
+G=(1,2,2,3,3)#Tupple
+
+H={"Name":["Shravan","Sahil"],"Rollno":[12,23]}#Dictionaries
+H["STD"]=["B","B"]# Key Add karna hoto
+H["Name"].append("Suaysh")#Agar value add karni hoto
+print(H)
